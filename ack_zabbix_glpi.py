525f9f4188b6b7eabaaa174c1c79c5e28a98b6d6
Mmm <mob.acc12@gmail.com>
2019-12-26 16:11:47 +0300

    translate to english

--- ack_zabbix_glpi.py
+++ ack_zabbix_glpi.py
@@ -1,6 +1,9 @@
 ## Autor: Janssen dos Reis Lima / janssenreislima@gmail.com>
 ## Ultima atualizacao: 18/02/2016
 ## Observacoes: Este script eh executado automaticamente apos a abertura do ticket no GLPI
+##
+## 2019/Dec/26
+
 
 from zabbix_api import ZabbixAPI
 import sys
@@ -9,7 +12,7 @@ server = "http://localhost/zabbix"
 username = "Admin"             
 password = "zabbix"     
  
-conexao = ZabbixAPI(server = server)
-conexao.login(username, password)
+connection = ZabbixAPI(server = server)
+connection.login(username, password)
 
-reconhecer_evento = conexao.event.acknowledge({"eventids": sys.argv[1], "message": "Ticket " + str(sys.argv[2]) + " criado no GLPI."})
+ack = connection.event.acknowledge({"eventids": sys.argv[1], "message": "Ticket " + str(sys.argv[2]) + " created in GLPI."})
